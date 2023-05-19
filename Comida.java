2a23caf6009ed64a4e70c3bcac4124c4c67c06d5
nicollegordillo <98134296+nicollegordillo@users.noreply.github.com>
2023-05-19 10:24:08 -0600

    Clases

---
+++ Comida.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Comida {
+    private String Nombre;
+    private String Tipo;
+    ArrayList<String> Ingredientes = new ArrayList<>();
+    ArrayList<String> IngredientesExcluidos = new ArrayList<>();
+    ArrayList<String> Recomendaciones = new ArrayList<>();
+
+    public void agregarRecomendacion(String Comida){
+        
+    }
+}
---
+++ GrafoComidas.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class GrafoComidas {
+    private ArrayList<Comida> comidas = new ArrayList<>();
+    
+    public void agregarComida(Comida Comida){
+
+    }
+
+    public ArrayList<String> buscarComidaPorTipo(String Tipo){
+        return null;
+        
+        
+    }
+
+    public Comida buscarComidaPorNombre(String Nombre){
+        return null;
+
+    }
+
+    public ArrayList<String> generarRecomendacion(String Tipo, ArrayList<String> Ingredientes, ArrayList<String> IngredientesExcluidos, String Nombre){
+        return null;
+        
+        
+    }
+
+}
